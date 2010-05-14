a150c50048c51d12528b86e1b254ffba5504b466
ibustama <devnull@localhost>
2010-05-14 13:26:33 -0400

    Se crea clase TweetSaver para mostrar en pantalla lo filtrado

--- chile_mysql.py
+++ chile_mysql.py
@@ -20,31 +20,42 @@ import logging
 logging.basicConfig(level=logging.DEBUG)
 log = logging.getLogger()
 
-class StreamWatcherListener(tweepy.StreamListener):
+class TweetSaverLog(object):
     status_wrapper = TextWrapper(width=70,
                                  initial_indent=' ',
                                  subsequent_indent=' ')
+
+    def save_raw_tweet( self, text, author, tweet_id, source, time ):
+        log.info(self.status_wrapper.fill(text))                            
+        log.info('%s %s via %s #%s\n' % ( 
+                                       author,
+                                       time,
+                                       source,
+                                       tweet_id))
+
+
+class StreamWatcherListener(tweepy.StreamListener):
     prog = re.compile(SECONDARY_REGEX_FILTER,
                       re.IGNORECASE)
 
-    def __init__(self, u, p):
+    def __init__(self, u, p, saver):
         self.auth = tweepy.BasicAuthHandler(username = u,
                                             password = p)
         self.api = tweepy.API(auth_handler = self.auth,
                               secure=True,
                               retry_count=3)
+        self.saver = saver
 
     def on_status(self, status):
         log.debug(status.text)
 
         if self.prog.search(status.text):
 
-            log.info(self.status_wrapper.fill(status.text))
-            log.info('%s %s via %s #%s\n' % (status.author.screen_name,
-                                       status.created_at,
-                                       status.source,
-                                       status.id))
-
+            self.saver.save_raw_tweet(status.text,
+                                        status.author.screen_name,                                                      
+                                        status.id,
+                                        status.source,                                                   
+                                        status.created_at)
 
     def on_limit(self, track):
         log.error('Limit hit! Track = %s' % track)
@@ -60,8 +71,9 @@ def main():
     try:
         username = 'TtTReadChile'
         password = 'TtT4Chile'
-
-        listener = StreamWatcherListener(username, password)
+        
+        saver = TweetSaverLog()
+        listener = StreamWatcherListener(username, password, saver)
         stream = tweepy.Stream(username,
                                password,
                                listener,
