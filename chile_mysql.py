91607044a26dc2819c18b62b40b1db4554b31811
ibustama <devnull@localhost>
2010-06-11 12:00:13 -0400

    + Tweetsaver modularizado Se empezó a utilizar .ini

--- chile_mysql.py
+++ chile_mysql.py
@@ -9,6 +9,7 @@
 from textwrap import TextWrapper
 import tweepy
 import re
+import tweetsavers
 
 # Primary Filter, can be a comma seperated list.
 PRIMARY_TRACK_LIST = "#chile, #earthquake, #tsunami, #terremotochile, #fuerzachile"
@@ -18,59 +19,7 @@ SECONDARY_REGEX_FILTER = '#(need|offer|have|closed|open|taybien|toybien|tavivo|n
 
 import logging
 logging.basicConfig(level=logging.DEBUG)
-log = logging.getLogger()
-
-class TweetSaverLog(object):
-    status_wrapper = TextWrapper(width=70,
-                                 initial_indent=' ',
-                                 subsequent_indent=' ')
-
-    def save_raw_tweet( self, text, author, tweet_id, source, time ):
-        log.info(self.status_wrapper.fill(text))                            
-        log.info('%s %s via %s #%s\n' % ( 
-                                       author,
-                                       time,
-                                       source,
-                                       tweet_id))
-
-import csv, os
-class TweetSaverCSV(object):
-    def __init__(self, filename):
-        self.filename = filename
-        if not os.path.isfile(self.filename):
-            self.writerow(['text', 'author', 'tweet_id', 'source', 'time'])
-
-    def writerow(self,row):
-        with open(self.filename, 'a') as destfile:
-            writer = csv.writer(destfile, delimiter=';', dialect='excel')
-            writer.writerow(row)
-    
-    def save_raw_tweet(self, text, author, tweet_id, source, time ):
-        self.writerow([text, author, tweet_id, source, time])
-
-import MySQLdb
-import traceback,sys
-class TweetSaverMySQL(object):
-    def __init__(self, host, db, user, passwd):
-        self.db =  MySQLdb.connect(host = host, user = user ,passwd = passwd ,db = db)
-        self.cursor = self.db.cursor()
-
-    def save_raw_tweet(self, text, author, tweet_id, source, time ):
-        sql = "INSERT INTO tweets (text, author, tweet_id, source, time) VALUES  ('%s', '%s', '%s', '%s', '%s' )" %  (text, author, tweet_id, source, time)
-        try:
-            # Execute the SQL command
-            self.cursor.execute(sql)
-            # Commit your changes in the database
-            self.db.commit()
-            print "Commit"
-
-        except:
-            # Rollback in case there is any error
-            self.db.rollback()
-            print "Rollback"
-            traceback.print_exc(file=sys.stdout)
-
-
+log = logging.getLogger(__name__)
 
 class StreamWatcherListener(tweepy.StreamListener):
     prog = re.compile(SECONDARY_REGEX_FILTER,
@@ -105,20 +54,30 @@ class StreamWatcherListener(tweepy.StreamListener):
     def on_timeout(self):
         log.error('Timeout: Snoozing Zzzzzz')
 
+import ConfigParser
+
 def main():
     try:
-        username = 'TtTReadChile'
-        password = 'TtT4Chile'
+        config = ConfigParser.ConfigParser()
+        config.read('config-chile.ini')
+        username = config.get('CONFIG','twitter_user')
+        password = config.get('CONFIG','twitter_pass')
         
-        log_saver = TweetSaverLog()
-        csv_saver = TweetSaverCSV('tweets.csv')
-        mysql_saver = TweetSaverMySQL('localhost', 'test', 'tweakthetweet', 'tweakthetweet')
+        #connectors = config.get('CONFIG','connectors')
+        #connectors = connectors.split(' ')
+        #log.debug(connectors)
+        
+        #savers = []
+        
+        log_saver = tweetsavers.TweetSaverLog(logging.getLogger('tweets'))
+        csv_saver = tweetsavers.TweetSaverCSV('tweets.csv')
+        mysql_saver = tweetsavers.TweetSaverMySQL('localhost', 'test', 'tweakthetweet', 'tweakthetweet')
         listener = StreamWatcherListener(username, password, [log_saver, csv_saver, mysql_saver])
         stream = tweepy.Stream(username,
                                password,
                                listener,
                                timeout = None)
-        track_list = [k for k in PRIMARY_TRACK_LIST.split(',')]
+        track_list = [k.strip() for k in PRIMARY_TRACK_LIST.split(',')]
 
         stream.filter(track = track_list)
     except KeyboardInterrupt:
---
+++ tweetsavers.py
@@ -0,0 +1,61 @@
+from textwrap import TextWrapper
+import logging
+logging.basicConfig(level=logging.DEBUG)
+log = logging.getLogger(__name__)
+
+class TweetSaverLog(object):
+    def __init__(self, log):
+        self.log = log
+
+    status_wrapper = TextWrapper(width=70,
+                                 initial_indent=' ',
+                                 subsequent_indent=' ')
+
+    def save_raw_tweet( self, text, author, tweet_id, source, time ):
+        self.log.info(self.status_wrapper.fill(text))                            
+        self.log.info('%s %s via %s #%s\n' % ( 
+                                       author,
+                                       time,
+                                       source,
+                                       tweet_id))
+
+import csv, os
+class TweetSaverCSV(object):
+    def __init__(self, filename):
+        self.filename = filename
+        if not os.path.isfile(self.filename):
+            self.writerow(['text', 'author', 'tweet_id', 'source', 'time'])
+
+    def writerow(self,row):
+        with open(self.filename, 'a') as destfile:
+            writer = csv.writer(destfile, delimiter=';', dialect='excel')
+            writer.writerow(row)
+    
+    def save_raw_tweet(self, text, author, tweet_id, source, time ):
+        self.writerow([text, author, tweet_id, source, time])
+
+import MySQLdb
+import traceback,sys
+class TweetSaverMySQL(object):
+    def __init__(self, host, db, user, passwd):
+        self.db =  MySQLdb.connect(host = host, user = user ,passwd = passwd ,db = db)
+        self.cursor = self.db.cursor()
+
+    def save_raw_tweet(self, text, author, tweet_id, source, time ):
+        sql = "INSERT INTO tweets (text, author, tweet_id, source, time) VALUES  ('%s', '%s', '%s', '%s', '%s' )" %  (text, author, tweet_id, source, time)
+        try:
+            # Execute the SQL command
+            self.cursor.execute(sql)
+            # Commit your changes in the database
+            self.db.commit()
+            log.info('MySQL Commit')
+
+        except:
+            # Rollback in case there is any error
+            self.db.rollback()
+            log.info('MySQL Rollback')
+            traceback.print_exc(file=sys.stdout)
+            
+
+
+
