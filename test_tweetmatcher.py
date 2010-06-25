faa237a3d918fe89b344c6b56e52a2f7e1449a29
pablo.cordella <devnull@localhost>
2010-06-25 11:20:45 -0400

    Ahora si

--- test_tweetmatcher.py
+++ test_tweetmatcher.py
@@ -5,21 +5,25 @@ class TestTweetMatcher(unittest.TestCase):
 	def setUp(self):
 		self.expected_requests = ["#busco", "#sebusca", "#ruok", "#need"]
 		self.expected_responses = ["#encontre","#seencontro","#imok","#found"]
-		self.tweet_matcher = tweetmatcher.TweetMatcher(self.expected_requests, self.expected_responses)
+		self.expected_location = ["#sitio","#location","#en","#loc","#zona","#localidad","#gps"," #lugar"]
+		self.expected_contact = ["#contacto","#contact","#contactar","#con","#cont","#avisar","#telefono","#fono","#tel","#cel"]
+		self.expected_name = ["#name","#nombre"]
+		self.tweet_matcher = tweetmatcher.TweetMatcher(self.expected_requests, self.expected_responses,self.expected_location,self.expected_contact,self.expected_name)
 	
 	def test_creatematcher(self):
 		self.assertEquals (self.expected_requests, self.tweet_matcher.requests)
 		self.assertEquals (self.expected_responses, self.tweet_matcher.responses)
+		self.assertEquals (self.expected_location, self.tweet_matcher.location)
+		self.assertEquals (self.expected_contact, self.tweet_matcher.contact)
+		self.assertEquals (self.expected_name, self.tweet_matcher.name)
 		
 	def test_succesful_storing(self):	
 		test_tweet_request = "#chile #sebusca Novia #contacto Pablo Cordella #location Martin Fierro 1234 #info buena presencia #status urgente"
 		test_tweet_response = "#chile #encontre Novia #contacto Felipe Garrido #location Principe de Gales"
-		#seria ideal que esta cosa haga un tweet con referencia a ambos respecto al match
-		#ie: "@pcordell @fgarrido hay match de Novia 1313"
 		self.tweet_matcher.parse(test_tweet_request)
 		self.tweet_matcher.parse(test_tweet_response)
-		assert test_tweet_request in self.tweet_matcher.parsed_requests
-		assert test_tweet_response in self.tweet_matcher.parsed_responses
+		assert "novia" in self.tweet_matcher.parsed_responses[0]
+		assert "novia" in self.tweet_matcher.parsed_requests[0]
 
 	def test_succesful_matching(self):	
 		test_tweet_request = "#chile #sebusca Novia #contacto Pablo Cordella #location Martin Fierro 1234 #info buena presencia #status urgente"
@@ -28,4 +32,28 @@ class TestTweetMatcher(unittest.TestCase):
 		assert not self.tweet_matcher.parse(test_tweet_request)
 		assert self.tweet_matcher.parse(test_tweet_response)
 		
+	def test_succesful_secondary_storage(self):
+	    #seria ideal que esta cosa haga un tweet con referencia a ambos respecto al match
+		#ie: "@pcordell @fgarrido hay match de Novia 1313"""
+		test_tweet_request = "#chile #sebusca Novia #contacto Pablo Cordella #location Martin Fierro 1234 #info buena presencia #status urgente"
+		test_tweet_response = "#chile #encontre Novia #contacto Felipe Garrido #location Principe de Gales"
 		
+		self.tweet_matcher.parse(test_tweet_request)
+		self.tweet_matcher.parse(test_tweet_response)
+		print self.tweet_matcher.parsed_responses
+		print self.tweet_matcher.parsed_requests
+		assert "felipe garrido" in self.tweet_matcher.parsed_responses[0][2]
+		assert "pablo cordella" in self.tweet_matcher.parsed_requests[0][2]
+		
+	def test_succesful_secondary_storage(self):
+		#seria ideal que esta cosa haga un tweet con referencia a ambos respecto al match
+		#ie: "@pcordell @fgarrido hay match de Novia 1313 """
+		test_tweet_request = "#chile #sebusca Novia #contacto Pablo Cordella #location Martin Fierro 1234 #info buena presencia #status urgente"
+		test_tweet_response = "#chile #encontre Novia #contacto Felipe Garrido #location Principe de Gales"
+		self.tweet_matcher.parse(test_tweet_request)
+		self.tweet_matcher.parse(test_tweet_response)
+		print self.tweet_matcher.parsed_responses
+		print self.tweet_matcher.parsed_requests
+		assert "felipe garrido" in self.tweet_matcher.parsed_responses[0][2]
+		assert "pablo cordella" in self.tweet_matcher.parsed_requests[0][2]
+	
--- tweetmatcher.py
+++ tweetmatcher.py
@@ -1,13 +1,51 @@
+import re
 class TweetMatcher(object):
 	
-	def __init__(self, requests, responses):
+	def __init__(self, requests, responses, location, contact, name):
 		self.requests = requests
 		self.responses = responses
+		self.location= location
+		self.contact= contact
+		self.name = name
 		self.parsed_requests = []
 		self.parsed_responses = []
 		
 	def parse(self, tweet):
-		if self.is_request(tweet):
-			pass
-		for itera_tweet in self.parsed_requests:
-			pass
+		result = self.get_value(tweet, self.requests)
+		if result:						
+			self.parsed_requests.append(self.append_info(tweet, result))
+			matches = []
+			for itera_tweet in self.parsed_responses:
+				if itera_tweet[0] == result:
+					matches.append(itera_tweet)
+			return matches
+		result = self.get_value(tweet, self.responses)
+		if result:					
+			self.parsed_responses.append(self.append_info(tweet, result))
+			matches = []
+			for itera_tweet in self.parsed_requests:
+				if itera_tweet[0] == result:
+					matches.append(itera_tweet)
+			return matches
+		return []
+			
+	def get_value(self, tweet, type):
+		result = re.search('('+ (' |').join(type) + ')([^#]*)', tweet)
+		if result:
+			return result.group(2).strip().lower()
+		return ""
+		
+	
+	def secondary_info(self, tweet, str):
+		result = re.search('('+ ('|').join(str) + ')([^#]*)(.*)', tweet)
+		if result:
+			return result.group(3).strip().lower()
+		return ""
+		
+	def append_info(self, tweet, result):
+		result_listed_info = []			
+		result_listed_info.append(result)
+		result_listed_info.append(self.get_value(tweet, self.location))
+		result_listed_info.append(self.get_value(tweet, self.contact))
+		result_listed_info.append(self.get_value(tweet, self.name))
+		return result_listed_info
