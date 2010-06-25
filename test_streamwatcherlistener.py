c98a7bd1c93b3c31dad73c41d08e413be9b237d8
Felipe Garrido <fgarrido@dcc.uchile.cl>
2010-06-25 11:29:14 -0400

    tests streamwatcherlistener completo

--- test_streamwatcherlistener.py
+++ test_streamwatcherlistener.py
@@ -7,23 +7,26 @@ class TestStreamWatcherListener(unittest.TestCase):
 	
 	def setUp(self):
 		self.saver=mock.Mock()
+		self.listener=cm.StreamWatcherListener(["#secondary1","#secondary2"],"TtTReadChile","TtT4Chile",[self.saver])
 		
-	def test_save_matched_tweet(self):
-		primary_tags=["#primary1","primary2"]
-		listener=cm.StreamWatcherListener(["#secondary1","#secondary2"],"TtTReadChile","TtT4Chile",[self.saver])
-		
+	def test_save_matched_tweet_succesful(self):
 		status1=mock.Mock()
-		status1.text="#primary1 este no se guarda"
-		listener.on_status(status1)
+		status1.text="#primary2 este se guarda #secondary1"
+		self.listener.on_status(status1)
+		self.assertEquals(self.saver.save_raw_tweet.call_count,1)
+		
+		status2=mock.Mock()
+		status2.text="#secondary2 este deberia pasar"
+		self.listener.on_status(status2)
+		self.assertEquals(self.saver.save_raw_tweet.call_count,2)
+
+	def test_save_matched_tweet_failed(self):
+		status1=mock.Mock()
+		status1.text="#primary2 este no se guarda"
+		self.listener.on_status(status1)
 		self.assertEquals(self.saver.save_raw_tweet.call_count,0)
 		
 		status2=mock.Mock()
-		status2.text="#primary2 este se guarda #secondary1"
-		listener.on_status(status2)
-		self.assertEquals(self.saver.save_raw_tweet.call_count,1)
-		
-		status3=mock.Mock()
-		status3.text="#secondary2 este deberia pasar"
-		listener.on_status(status3)
-		self.assertEquals(self.saver.save_raw_tweet.call_count,2)
-		
+		status2.text="#primary1 tampoco se guarda"
+		self.listener.on_status(status2)
+		self.assertEquals(self.saver.save_raw_tweet.call_count,0)
