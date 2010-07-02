3dd469a54b89759576e8d4a43026dee31729beaf
Felipe Garrido <fgarrido@dcc.uchile.cl>
2010-07-02 13:12:34 -0400

    test de inireader OK

--- test_inireader.py
+++ test_inireader.py
@@ -1,4 +1,4 @@
-from chile_mysql import IniReader
+from ini_reader import IniReader
 import unittest
 
 class TestIniReader(unittest.TestCase):
@@ -11,4 +11,31 @@ class TestIniReader(unittest.TestCase):
 		
 	def test_get_primary_tags(self):
 		self.assertEquals(self.ini.get_primary_tag_list(),["#chile","#terremoto","#fmarepoto"])
+	
+	def test_get_secondary_tag_list(self):
+		expected=["#estaibien","#missing","#ruok","#sebusca","#estaibien","#buscapersona","#estasbien","#tavivo","#estabien","#tabien","#estoybien","#todobien","#toybien","#imok","#survivor","#encontr","#found","#necesita","#need","#necesidad","#donacion","#ofrece","#tenemos","#tengo","#have","#offer","#dono","#closed","#open","#damage","#evac","#edificiocolapso"]
+		expected.sort()
+		got=self.ini.get_secondary_tag_list()
+		got.sort()
+		self.assertEquals(got,expected)
+	
+	def test_get_categories_req_resp(self):
+		expected=dict([])
+		expected[('PEOPLE','request')]=	["#estaibien","#missing","#ruok","#sebusca","#estaibien","#buscapersona","#estasbien"]
+		expected[('PEOPLE','response')]=["#tavivo","#estabien","#tabien","#estoybien","#todobien","#toybien","#imok","#survivor","#encontr","#found"]
+		expected[('HELP','request')]=["#necesita","#need","#necesidad"]
+		expected[('HELP','response')]=["#donacion","#ofrece","#tenemos","#tengo","#have","#offer","#dono"]
+		expected[('INFO','request')]=[]
+		expected[('INFO','response')]=["#closed","#open","#damage","#evac","#edificiocolapso"]
+		self.assertEquals(self.ini.get_categories_req_resp(),expected)
 
+	def test_get_categories(self):
+		self.assertEquals(self.ini.get_categories(),["PEOPLE","HELP","INFO"])
+
+	def test_get_mysqlinfo(self):
+		self.assertEquals(self.ini.get_mysql_info(),["myhost","myport","myuser","mypass"])
+	
+	def test_get_info_tags(self):
+		self.assertEquals(self.ini.get_info_contact(), ["#contacto","#contact", "#contactar", "#con", "#cont", "#avisar", "#telefono", "#fono", "#tel", "#cel"])
+		self.assertEquals(self.ini.get_info_location(), ["#sitio","#location","#en","#loc","#zona","#localidad","#gps","#lugar"])
+		self.assertEquals(self.ini.get_info_name(),["#name","#nombre"])
