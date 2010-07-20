5be609c9014ec30285bd50be66cd4861c1a74ff7
Felipe Garrido <fgarrido@dcc.uchile.cl>
2010-07-20 13:35:34 -0400

    cambios anteriores

--- tweetdispatcher.py
+++ tweetdispatcher.py
@@ -5,53 +5,58 @@ import pdb
 
 class TweetDispatcher(object):
 
-    def __init__(self, file_path):
+	def __init__(self, file_path):
 
-        ini = IniReader(file_path)
-        mysql_info = ini.get_mysql_info()
-        mysql_host = mysql_info[0]
-        mysql_port = mysql_info[1]
-        mysql_user = mysql_info[2]
-        mysql_pass = mysql_info[3]
-        mysql_db_name = mysql_info[4]
-
-
-
-        categorias = ini.get_categories()
-        diccionario = ini.get_categories_req_resp()
-
-        location = ini.get_info_location()
-        contact = ini.get_info_contact()
-        name = ini.get_info_name()
-
-        for categoria in categorias:
-
-            requests = diccionario[(categoria, 'request')]
-            responses = diccionario[(categoria, 'response')]
-
-            tweet_matcher = tweetmatcher.TweetMatcher(requests, responses,location,contact,name)
-
-            tweets = self.tweet_reader(mysql_host, mysql_user, mysql_pass, mysql_db_name)
-            rows = tweets.fetch_row(0)
-            for tweet in rows: #tweet[0] = id, #tweet[1] = text
-                #print tweet
-                tupla  = [tweet[0], tweet[1]]
-                text = tweet_matcher.parse(tupla)
-                if ( text != [] ):
-                    #TODO: hacer update a los tweets con el id, que esta en text[0]
-                    #acutalizar el dispatcher para leer del diccionario
-                    #modificar el mysql para que lea los parametros de la bd del .ini
-                    #ponerse de acuerdo que hacer cuando se hace un match
-                    print text
+		ini = IniReader(file_path)
+		mysql_info = ini.get_mysql_info()
+		mysql_host = mysql_info[0]
+		mysql_port = mysql_info[1]
+		mysql_user = mysql_info[2]
+		mysql_pass = mysql_info[3]
+		mysql_db_name = mysql_info[4]
 
 
-    def tweet_reader(self, host, user, passwd, db):
-        self.db =  MySQLdb.connect(host, user, passwd, db)
-        sql = "SELECT id, text FROM  tweets"
-        self.db.query(sql)
-        return self.db.store_result()
 
-    def tweet_match(self, host, user, passwd, db, request_id, response_id):
-        self.db =  MySQLdb.connect(host, user, passwd, db)
-        sql = "INSERT INTO tweet_matched(request_id, response_id) VALUES(%i, %i);" % (request_id, response_id)
-        self.db.query(sql)
+		categorias = ini.get_categories()
+		diccionario = ini.get_categories_req_resp()
+
+		location = ini.get_info_location()
+		contact = ini.get_info_contact()
+		name = ini.get_info_name()
+
+		for categoria in categorias:
+
+			requests = diccionario[(categoria, 'request')]
+			responses = diccionario[(categoria, 'response')]
+
+			tweet_matcher = tweetmatcher.TweetMatcher(categoria, requests, responses,location,contact,name,[mysql_host,mysql_user,mysql_pass,mysql_db_name])
+
+			tweets = self.tweet_reader(mysql_host, mysql_user, mysql_pass, mysql_db_name)
+			rows = tweets.fetch_row(0)
+			for tweet in rows: #tweet[0] = id, #tweet[1] = text
+				tupla  = [tweet[0], tweet[1], tweet[2]]
+				[type,text] = tweet_matcher.parse(tupla)
+				if text:
+					#TODO: hacer update a los tweets con el id, que esta en text[0]
+					#acutalizar el dispatcher para leer del diccionario
+					#modificar el mysql para que lea los parametros de la bd del .ini
+					#ponerse de acuerdo que hacer cuando se hace un match
+					if type==tweetmatcher.REQ:
+						map(lambda x:self.tweet_match(mysql_host,mysql_user,mysql_pass,mysql_db_name,x,tweet[0]),text)
+					elif type==tweetmatcher.RESP:
+						map(lambda x:self.tweet_match(mysql_host,mysql_user,mysql_pass,mysql_db_name,tweet[0],x),text)
+
+
+	def tweet_reader(self, host, user, passwd, db):
+		self.db =  MySQLdb.connect(host, user, passwd, db)
+		sql = "SELECT tweet_id, text, time FROM tweets"
+		self.db.query(sql)
+		return self.db.store_result()
+
+	def tweet_match(self, host, user, passwd, db, request_id, response_id):
+		self.db =  MySQLdb.connect(host, user, passwd, db)
+		sql = "INSERT INTO tweets_matched(request_id, response_id,date) VALUES(%s, %s,NOW());" % (request_id, response_id)
+		self.db.query(sql)
+
+if __name__ == '__main__':
+	asdf=TweetDispatcher("config-chile.ini")
--- tweetmatcher.py
+++ tweetmatcher.py
@@ -1,34 +1,37 @@
+import MySQLdb
 import re
+
+REQ=1
+RESP=2
+
 class TweetMatcher(object):
-	
-	def __init__(self, requests, responses, location, contact, name):
+
+	def __init__(self, category,requests, responses, location, contact, name, db_conf):
+		self.category=category
 		self.requests = requests
 		self.responses = responses
 		self.location= location
 		self.contact= contact
 		self.name = name
-		self.parsed_requests = []
-		self.parsed_responses = []
+		self.dbconf=db_conf
+#		self.parsed_requests = []
+#		self.parsed_responses = []
 		
 	def parse(self, tupla_tweet):
 		tweet = tupla_tweet[1]
-		result = self.get_value(tweet, self.requests) #si es request y result = novia
+		result = self.get_value(tweet, self.requests) #si el tweet es un request, entonces result = novia
 		if result:
-			self.parsed_requests.append(self.append_info(tupla_tweet, result))
-			matches = []
-			for itera_tweet in self.parsed_responses:
-				if itera_tweet[1] == result:					
-					matches.append(itera_tweet)
-			return matches
+			self.insert_bd('request',result,tupla_tweet) # insertar en bd
+			matches = self.get_match_bd('response',result)
+			matches = map(lambda x:x[0],matches.fetch_row(0))
+			return [REQ,matches]
 		result = self.get_value(tweet, self.responses)
 		if result:					
-			self.parsed_responses.append(self.append_info(tupla_tweet, result))
-			matches = []
-			for itera_tweet in self.parsed_requests:
-				if itera_tweet[1] == result:
-					matches.append(itera_tweet)
-			return matches
-		return []
+			self.insert_bd('response',result,tupla_tweet)
+			matches = self.get_match_bd('request',result)
+			matches = map(lambda x:x[0],matches.fetch_row(0))
+			return [RESP,matches]
+		return [None,None]
 			
 	def get_value(self, tweet, type):
 		result = re.search('('+ (' |').join(type) + ')([^#]*)', tweet)
@@ -43,13 +46,19 @@ class TweetMatcher(object):
 			return result.group(3).strip().lower()
 		return ""
 		
-	def append_info(self, tweet, result):
-		result_listed_info = []			
-		result_listed_info.append(tweet[0])
-		result_listed_info.append(result)
-		result_listed_info.append(self.get_value(tweet[1], self.location))
-		result_listed_info.append(self.get_value(tweet[1], self.contact))
-		result_listed_info.append(self.get_value(tweet[1], self.name))		
+	def insert_bd(self, type, info, tweet):
+		categ=self.category
+		loc=self.get_value(tweet[1],self.location)
+		cont=self.get_value(tweet[1],self.contact)
+		name=self.get_value(tweet[1],self.name)
+		db =  MySQLdb.connect(self.dbconf[0], self.dbconf[1], self.dbconf[2], self.dbconf[3])
+		sql = "INSERT INTO tweets_parsed(id_tweet,type,category,info,contact,location,name,time) VALUES(%s,'%s','%s','%s','%s','%s','%s','%s');" % (tweet[0],type,categ,info,cont,loc,name,tweet[2])
+		db.query(sql)
 		
-		
-		return result_listed_info
+	def get_match_bd(self,type,info):
+		db =  MySQLdb.connect(self.dbconf[0], self.dbconf[1], self.dbconf[2], self.dbconf[3])
+		info="%"+info+"%"
+		sql = "SELECT id_tweet FROM tweets_parsed WHERE type='%s' AND info LIKE '%s';" % (type,info)
+		db.query(sql)
+		return db.store_result()
+
